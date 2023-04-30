f427b2029677a99746a1dfb9794bd8126d7bed84
unknown <njax30@gmail.com>
2023-04-30 12:56:38 +0300

    add database to project

--- main.go
+++ main.go
@@ -1,5 +1,23 @@
 package main
 
-func main() {
+import (
+	"fmt"
+	_ "github.com/mattn/go-sqlite3"
+	"log"
+	"net/http"
+)
 
+func main() {
+	http.HandleFunc("/hello", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintf(w, "Hello!")
+	})
+
+	http.HandleFunc("/solk", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintf(w, "Hello solk!")
+	})
+
+	fmt.Printf("Starting server at port 8080\n")
+	if err := http.ListenAndServe(":8080", nil); err != nil {
+		log.Fatal(err)
+	}
 }
